ce4cdb92841d89f9cb024d21e6ae11bc9f21190b
maharjananil <maharjananil1@gmail.com>
2023-10-09 23:33:20 +0545

    register front end design

--- exam_portal_frontend/src/app/app.component.ts
+++ exam_portal_frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exam_portal_frontend';
+  title = 'Welcome to new world';
 }
